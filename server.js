79e438ed4515a01e96e95064593fe225f451e9c1
Peter <petecruckshank@gmail.com>
2018-11-27 21:23:13 -0500

    added db.connect && routes for user,profile, posts

--- server.js
+++ server.js
@@ -1,8 +1,29 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
+
 
 const app = express();
 
+// DB Config
+const db = require('./config/keys').mongoURI;
+
+// Connect to MongoDB
+mongoose
+  .connect(db)
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.warn(err));
+
 app.get('/', (req, res) => res.send('Hello'));
+
+// Use routes
+app.use('/api/users', users);
+app.use('/api/posts', posts);
+app.use('/api/profile', profile);
+
 
 const port = process.env.PORT || 5000;
 
