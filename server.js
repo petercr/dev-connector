daac25bfe26d6cb3bec5e1334081d19b67bb8b14
Peter <petecruckshank@gmail.com>
2018-11-29 20:14:29 -0500

    added in body-parser to handle URL encoding

--- server.js
+++ server.js
@@ -1,29 +1,35 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
-const users = require('./routes/api/users');
-const profile = require('./routes/api/profile');
-const posts = require('./routes/api/posts');
-
+const users = require("./routes/api/users");
+const profile = require("./routes/api/profile");
+const posts = require("./routes/api/posts");
 
 const app = express();
 
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 // DB Config
-const db = require('./config/keys').mongoURI;
+const db = require("./config/keys").mongoURI;
 
 // Connect to MongoDB
 mongoose
-  .connect(db, { useNewUrlParser: true })
-  .then(() => console.log('MongoDB Connected'))
+  .connect(
+    db,
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log("MongoDB Connected"))
   .catch(err => console.warn(err));
 
-app.get('/', (req, res) => res.send('Hello'));
+app.get("/", (req, res) => res.send("Hello"));
 
 // Use routes
-app.use('/api/users', users);
-app.use('/api/posts', posts);
-app.use('/api/profile', profile);
-
+app.use("/api/users", users);
+app.use("/api/posts", posts);
+app.use("/api/profile", profile);
 
 const port = process.env.PORT || 5000;
 
