a1eeb02e4abfa50012a68f9431274661ebda4ca4
Peter <petecruckshank@gmail.com>
2018-12-04 19:59:10 -0500

    brought in jsonwebtoken and the keys file for verification Also set up route for ./routes/login & hashed the pw

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const keys = require("../../config/keys");
 
 // Load user modal
 const User = require("../../modules/User");
@@ -44,6 +46,45 @@ router.post("/register", (req, res) => {
         });
       });
     }
+  });
+});
+
+// @route   GET api/users/login
+// @desc    Login User/ Return JWT
+// @access  Public
+router.post("/login", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  // Find user by email
+  User.findOne({ email }).then(user => {
+    // Check for user
+    if (!user) {
+      return res.status(404).json({ email: "User not found" });
+    }
+
+    // Check Password
+    bcrypt.compare(password, user.password).then(isMatch => {
+      if (isMatch) {
+        // User Matched
+        const payLoad = { id: user.id, name: user.name, avatar: user.avatar }; // Create JWT payload
+
+        // Sign Token
+        jwt.sign(
+          payLoad,
+          keys.secretOrKey,
+          { expiresIn: 3600 },
+          (err, token) => {
+            res.json({
+              success: true,
+              token: "Bearer " + token
+            });
+          }
+        );
+      } else {
+        return res.status(400).json({ password: "Password Incorrect" });
+      }
+    });
   });
 });
 
