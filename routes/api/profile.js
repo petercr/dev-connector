22862b97baa0f8aff026cfc7840c1fcc5183da10
Peter <petecruckshank@gmail.com>
2018-12-22 14:21:32 -0500

    added new route for "/" to check validation And if profile of user actually exists

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,10 +1,37 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+const passport = require("passport");
+
+// Load profile model
+const Profile = require("../../modules/Profiles");
+// Load user profile
+const User = require("../../modules/User");
 
 // @route   GET api/profile/test
 // @desc    Tests profile route
 // @access  Public
 
-router.get('/test', (req, res) => res.json({msg: "Profile works!!"}));
+router.get("/test", (req, res) => res.json({ msg: "Profile works!!" }));
+
+// @route   GET api/profile
+// @desc    Get current users profile
+// @access  Private
+router.get(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const errors = {};
+    Profile.findOne({ user: req.user.id })
+      .then(profile => {
+        if (!profile) {
+          errors.noprofile = "There is no profile for this user";
+          return res.status(404).json(errors);
+        }
+        res.status(200).json(profile);
+      })
+      .catch(err => res.status(404).json(err));
+  }
+);
 
 module.exports = router;
