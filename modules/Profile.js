1dd9b38ac0844a51fa5332ed74b6730cf89a000b
Peter <petecruckshank@gmail.com>
2018-12-26 20:59:40 -0500

    changed name to Profile.js

--- modules/Profile.js
+++ modules/Profile.js
@@ -5,7 +5,7 @@ const Schema = mongoose.Schema;
 const ProfileSchema = new Schema({
   user: {
     type: Schema.Types.ObjectId,
-    refs: "users"
+    ref: "users"
   },
   handle: {
     type: String,
