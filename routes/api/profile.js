2cfe9839647e77bfe19118988045c7789a8976f4
Peter <petecruckshank@gmail.com>
2018-12-27 20:03:12 -0500

    switched ./api/profile session: true and it seems to have fixed problem Can now updated multiple user profiles & get all profiles

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -20,24 +20,20 @@ router.get("/test", (req, res) => res.json({ msg: "Profile works!!" }));
 // @route   GET api/profile
 // @desc    Get current users profile
 // @access  Private
-router.get(
-  "/",
-  passport.authenticate("jwt", { session: false }),
-  (req, res) => {
-    const errors = {};
+router.get("/", passport.authenticate("jwt", { session: true }), (req, res) => {
+  const errors = {};
 
-    Profile.findOne({ user: req.user.id })
-      .populate("user", ["name", "avatar"])
-      .then(profile => {
-        if (!profile) {
-          errors.noprofile = "There is no profile for this user";
-          return res.status(404).json(errors);
-        }
-        res.status(200).json(profile);
-      })
-      .catch(err => res.status(404).json(err));
-  }
-);
+  Profile.findOne({ user: req.user.id })
+    .populate("user", ["name", "avatar"])
+    .then(profile => {
+      if (!profile) {
+        errors.noprofile = "There is no profile for this user";
+        return res.status(404).json(errors);
+      }
+      res.status(200).json(profile);
+    })
+    .catch(err => res.status(404).json(err));
+});
 
 // @route   GET api/profile/all
 // @desc    Get all profiles
