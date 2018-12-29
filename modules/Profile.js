9e1a1fb505b862874bf519902cefc07d2447a422
Peter <petecruckshank@gmail.com>
2018-12-28 20:43:19 -0500

    changed education.field to education.fieldofstudy due to typo

--- modules/Profile.js
+++ modules/Profile.js
@@ -74,7 +74,7 @@ const ProfileSchema = new Schema({
         type: String,
         required: true
       },
-      field: {
+      fieldofstudy: {
         type: String,
         required: true
       },
