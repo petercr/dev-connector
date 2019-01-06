79615004557ea3a57fec9e9cf487d67057a26639
Peter <petecruckshank@gmail.com>
2019-01-06 16:50:47 -0500

    init POST route for "./" so logged in user can post Also included Post validation

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,9 +1,42 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+const passport = require("passport");
+
+// Post Model
+const Post = require("../../modules/post");
+
+const validatePostInput = require("../../validation/post");
 
 // @route   GET api/posts/test
 // @desc    Tests post route
 // @access  Public
-router.get('/test', (req, res) => res.json({msg: "Posts works!!"}));
+router.get("/test", (req, res) => res.json({ msg: "Posts works!!" }));
+
+// @route   POST api/posts/posts
+// @desc    Create posts
+// @access  Private
+router.post(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validatePostInput(req.body);
+
+    // Check Validation
+    if (!isValid) {
+      // If any errors, send 400 with errors object
+      return res.status(400).json(errors);
+    }
+
+    const newPost = new Post({
+      text: req.body.text,
+      name: req.body.name,
+      avatar: req.body.avatar,
+      user: req.user.id
+    });
+
+    newPost.save().then(post => res.json(post));
+  }
+);
 
 module.exports = router;
