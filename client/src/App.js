c5a0169d5e09f1432c482351ac037de994abfe08
Peter <petecruckshank@gmail.com>
2019-01-16 20:13:07 -0500

    addded in Navbar, Landing, Footer components to app

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from "react";
 import "./App.css";
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
+import Landing from "./components/layout/Landing";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>My React App</h1>
+      <Navbar />
+      <Landing />
+      <Footer />
       </div>
     );
   }
