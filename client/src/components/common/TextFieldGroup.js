7bbaae4d2699d2c9a3c4b8eecf32e221ec0c66e3
Peter <petecruckshank@gmail.com>
2019-03-01 21:09:06 -0500

    fixed String: placeholder now in no longer required That fixed the bug!

--- client/src/components/common/TextFieldGroup.js
+++ client/src/components/common/TextFieldGroup.js
@@ -34,7 +34,7 @@ const TextFieldGroup = ({
 
 TextFieldGroup.propTypes = {
   name: PropTypes.string.isRequired,
-  placeholder: PropTypes.string.isRequired,
+  placeholder: PropTypes.string,
   value: PropTypes.string.isRequired,
   info: PropTypes.string,
   error: PropTypes.string,
