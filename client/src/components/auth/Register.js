d08dde2947039d61d4bb0bfa8a33c7f094675a76
Peter <petecruckshank@gmail.com>
2019-03-02 17:09:17 -0500

    fixed another typo: password2 is !type lol

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -91,7 +91,7 @@ class Register extends Component {
                 <TextFieldGroup
                   placeholder="Confirm Password"
                   name="password2"
-                  type="password2"
+                  type="password"
                   value={this.state.password2}
                   onChange={this.onChange}
                   error={errors.password2}
