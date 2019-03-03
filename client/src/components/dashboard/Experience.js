09386576eaf9e1f9f4da0e5a5ae3615921cf47c6
Peter <petecruckshank@gmail.com>
2019-03-03 17:08:59 -0500

    init <Experience /> to show previos Exp. as cards on <Dashboard />

---
+++ client/src/components/dashboard/Experience.js
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { withRouter } from "react-router-dom";
+import Moment from "react-moment";
+
+class Experience extends Component {
+  render() {
+    const experience = this.props.experience.map(exp => (
+      <tr key={exp._id}>
+        <td>{exp.company}</td>
+        <td>{exp.title}</td>
+        <td>
+          <Moment date={exp.from} format="YYYY/MM/DD" />
+        </td>
+        {console.log(exp)}
+        <td>
+          <button className="btn btn-danger">Delete</button>
+        </td>
+      </tr>
+    ));
+    return (
+      <div>
+        <h4 className="mb-4">Experience Credentials</h4>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Company</th>
+              <th>Title</th>
+              <th>Years</th>
+              <th />
+            </tr>
+            {experience}
+          </thead>
+        </table>
+      </div>
+    );
+  }
+}
+
+export default connect(null)(withRouter(Experience));
