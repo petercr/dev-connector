789d057f9e4379d24c6e5a3e10c1ae62b0c46b72
Peter <petecruckshank@gmail.com>
2019-03-06 15:47:30 -0500

    fixed Moment.js & added conditional to render comp Also onClick for Btn & pass deleteExperience via PropTypes

--- client/src/components/dashboard/Experience.js
+++ client/src/components/dashboard/Experience.js
@@ -1,21 +1,34 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
-import { withRouter } from "react-router-dom";
 import Moment from "react-moment";
+import { deleteExperience } from "../../actions/profileActions";
 
 class Experience extends Component {
+  onDeleteClick(id) {
+    this.props.deleteExperience(id);
+  }
+
   render() {
     const experience = this.props.experience.map(exp => (
       <tr key={exp._id}>
         <td>{exp.company}</td>
         <td>{exp.title}</td>
         <td>
-          <Moment date={exp.from} format="YYYY/MM/DD" />
+          <Moment date={exp.from} format="YYYY/MM/DD" /> -
+          {exp.to === null ? (
+            " Now"
+          ) : (
+            <Moment date={exp.to} format="YYYY/MM/DD" />
+          )}
         </td>
-        {console.log(exp)}
         <td>
-          <button className="btn btn-danger">Delete</button>
+          <button
+            onClick={this.onDeleteClick.bind(this, exp._id)}
+            className="btn btn-danger"
+          >
+            Delete
+          </button>
         </td>
       </tr>
     ));
@@ -38,4 +51,11 @@ class Experience extends Component {
   }
 }
 
-export default connect(null)(withRouter(Experience));
+Experience.propTypes = {
+  deleteExperience: PropTypes.func.isRequired
+};
+
+export default connect(
+  null,
+  { deleteExperience }
+)(Experience);
