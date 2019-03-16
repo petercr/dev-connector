5fcda38b01b386007e62da2786983db18c5adce0
Peter <petecruckshank@gmail.com>
2019-03-16 19:11:55 -0400

    set payload to NULL User gets routed to /not-found

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -42,7 +42,7 @@ export const getProfileByHandle = handle => dispatch => {
     .catch(err =>
       dispatch({
         type: GET_PROFILE,
-        payload: {}
+        payload: null
       })
     );
 };
