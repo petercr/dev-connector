a7f9621aa5e63edcc419e6f1528f69e65b1a0866
Peter <petecruckshank@gmail.com>
2019-03-15 21:33:39 -0400

    added routes for create&edit-profile Also add-experience & add-education All Private routes, except for /not-found

--- client/src/App.js
+++ client/src/App.js
@@ -22,6 +22,7 @@ import AddExperience from "./components/add-credentials/AddExperience";
 import AddEducation from "./components/add-credentials/AddEducation";
 import Profiles from "./components/profiles/Profiles";
 import Profile from "./components/profile/Profile";
+import NotFound from "./components/not-found/NotFound";
 
 import "./App.css";
 
@@ -64,33 +65,18 @@ class App extends Component {
                 <PrivateRoute exact path="/dashboard" component={Dashboard} />
               </Switch>
               <Switch>
-                <PrivateRoute
-                  exact
-                  path="/create-profile"
-                  component={CreateProfile}
-                />
+                <PrivateRoute exact path="/create-profile" component={CreateProfile} />
               </Switch>
               <Switch>
-                <PrivateRoute
-                  exact
-                  path="/edit-profile"
-                  component={EditProfile}
-                />
+                <PrivateRoute exact path="/edit-profile" component={EditProfile} />
               </Switch>
               <Switch>
-                <PrivateRoute
-                  exact
-                  path="/add-experience"
-                  component={AddExperience}
-                />
+                <PrivateRoute exact path="/add-experience" component={AddExperience} />
               </Switch>
               <Switch>
-                <PrivateRoute
-                  exact
-                  path="/add-education"
-                  component={AddEducation}
-                />
+                <PrivateRoute exact path="/add-education" component={AddEducation} />
               </Switch>
+              <Route exact path="/not-found" component={NotFound} />
             </div>
             <Footer />
           </div>
