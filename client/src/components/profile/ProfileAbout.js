6f123c5104f9920f02e0d31a72e9135838c9f974
Peter <petecruckshank@gmail.com>
2019-03-15 21:18:01 -0400

    added PropTypes & passed profile

--- client/src/components/profile/ProfileAbout.js
+++ client/src/components/profile/ProfileAbout.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 import isEmpty from "../../validation/isEmpty";
 
 class ProfileAbout extends Component {
@@ -41,5 +42,9 @@ class ProfileAbout extends Component {
     );
   }
 }
+
+ProfileAbout.propTypes = {
+  profile: PropTypes.object.isRequired
+};
 
 export default ProfileAbout;
