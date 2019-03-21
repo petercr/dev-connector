2a00e76732cd752f1fe3b41dd882544aadd95ffd
Peter <petecruckshank@gmail.com>
2019-03-21 16:08:00 -0400

    added getPosts() & setPostLoading() to actions

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 
-import { ADD_POST, GET_ERRORS } from "./types";
+import { ADD_POST, GET_ERRORS, GET_POSTS, POST_LOADING } from "./types";
 
-// Add post
+// Add Post
 export const addPost = postData => dispatch => {
+  // dispatch(clearErrors());
   axios
     .post("/api/posts", postData)
     .then(res =>
@@ -18,4 +19,30 @@ export const addPost = postData => dispatch => {
         payload: err.response.data
       })
     );
+};
+
+// Get Posts
+export const getPosts = () => dispatch => {
+  dispatch(setPostLoading());
+  axios
+    .get("/api/posts")
+    .then(res =>
+      dispatch({
+        type: GET_POSTS,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_POSTS,
+        payload: null
+      })
+    );
+};
+
+// Set loading state
+export const setPostLoading = () => {
+  return {
+    type: POST_LOADING
+  };
 };
