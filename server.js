1d0c945fc6bfc46ff9e6ea9f986c1165a8ae93ca
Peter <petecruckshank@gmail.com>
2019-03-26 17:19:35 -0400

    new comments

--- server.js
+++ server.js
@@ -39,6 +39,7 @@ if (process.env.NODE_ENV === "production") {
   // Set static folder
   app.use(express.static("client/build"));
 
+  // Change path to client/build
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
