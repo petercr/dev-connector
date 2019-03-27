6216ac7d07e50af4bc7dbbf817a5f1c998509c7d
Peter <petecruckshank@gmail.com>
2019-03-26 20:13:20 -0400

    maybe this does something???

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ if (process.env.NODE_ENV === "production") {
 
   // Change path to client/build
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "/client/build/index.html"));
   });
 }
 
