6159a1df1700f4ae5615928488919ff35b83e076
Peter <petecruckshank@gmail.com>
2019-03-27 11:06:19 -0400

    switch process.env.PORT to *.$PORT

--- server.js
+++ server.js
@@ -45,6 +45,6 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-const port = process.env.PORT || 5000;
+const port = process.env.$PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
