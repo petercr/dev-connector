8b504af3bc2497f7cbf048e7b991c05e2ecd20c2
Peter <petecruckshank@gmail.com>
2019-03-27 11:02:46 -0400

    changed back to just * for PROD express route

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 
   // Change path to client/build
-  app.get("/*", (req, res) => {
+  app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
