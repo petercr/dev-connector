534bbb213f46d77ea234e0cae827fa8521890701
Peter <petecruckshank@gmail.com>
2019-03-27 10:53:14 -0400

    tried to change app.get() to "/*"

--- server.js
+++ server.js
@@ -29,21 +29,21 @@ app.use(passport.initialize());
 // Passport Config
 require("./config/passport")(passport);
 
-// Server static assets if in production
-if (process.env.NODE_ENV === "production") {
-  // Set static folder
-  app.use(express.static(path.join(__dirname, "client/build")));
-
-  // Change path to client/build
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "/client/build/index.html"));
-  });
-}
-
 // Use routes
 app.use("/api/users", users);
 app.use("/api/posts", posts);
 app.use("/api/profile", profile);
+
+// Server static assets if in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  // Change path to client/build
+  app.get("/*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
