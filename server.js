7459edaf0624fd4f6f758e7d1e89fcd6f64900bd
Peter <petecruckshank@gmail.com>
2019-03-30 15:45:12 -0400

    copied Brad's server.js & + PROD switch

--- server.js
+++ server.js
@@ -9,9 +9,6 @@ const profile = require("./routes/api/profile");
 const posts = require("./routes/api/posts");
 
 const app = express();
-const port = process.env.PORT || 5000;
-
-const PROD = true;
 
 // Body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -20,11 +17,13 @@ app.use(bodyParser.json());
 // DB Config
 const db = require("./config/keys").mongoURI;
 
+const PROD = true;
+
 // Connect to MongoDB
 mongoose
   .connect(db)
   .then(() => console.log("MongoDB Connected"))
-  .catch(err => console.warn(err));
+  .catch(err => console.log(err));
 
 // Passport middleware
 app.use(passport.initialize());
@@ -32,20 +31,21 @@ app.use(passport.initialize());
 // Passport Config
 require("./config/passport")(passport);
 
-// Use routes
+// Use Routes
 app.use("/api/users", users);
-app.use("/api/posts", posts);
 app.use("/api/profile", profile);
+app.use("/api/posts", posts);
 
 // Server static assets if in production
 if (process.env.NODE_ENV === "production" || PROD) {
   // Set static folder
   app.use(express.static("client/build"));
 
-  // Change path to client/build
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
+
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
