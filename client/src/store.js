cf23fa538468aa8714f5585f97f9896fda768e88
Peter <petecruckshank@gmail.com>
2019-03-30 12:41:16 -0400

    applied fix to disable Redux dev tools: production

--- client/src/store.js
+++ client/src/store.js
@@ -6,13 +6,18 @@ const initialState = {};
 
 const middleware = [thunk];
 
+// Developer tools middleware
+const composeSetup =
+  process.env.NODE_ENV !== "production" &&
+  typeof window === "object" &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    : compose;
+
 const store = createStore(
   rootReducer,
   initialState,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  composeSetup(applyMiddleware(...middleware))
 );
 
 export default store;
